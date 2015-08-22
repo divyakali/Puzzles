3bc5209b5aaf06af182a38386f8bcdd7419eac07
aethena <divyakali1288@gmail.com>
2015-08-22 10:31:56 -0400

    initial commit with list node

---
+++ src/com/puzzles/ListNode.java
@@ -0,0 +1,51 @@
+package com.puzzles;
+
+/**
+ * Class to represent a list of nodes
+ * 
+ * @author aethena
+ *
+ */
+public class ListNode {
+	private int data;
+	private ListNode next;
+
+	/**
+	 * Constructor to create a node with data
+	 * @param data
+	 */
+	public ListNode(int data) {
+	}
+
+	/**
+	 * Get data
+	 * @return
+	 */
+	public int getData() {
+		return data;
+	}
+
+	/**
+	 * Set data
+	 * @param data
+	 */
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	/**
+	 * Get next node
+	 * @return
+	 */
+	public ListNode getNext() {
+		return next;
+	}
+
+	/**
+	 * Set next node
+	 * @param next
+	 */
+	public void setNext(ListNode next) {
+		this.next = next;
+	}
+}
